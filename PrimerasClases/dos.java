87c7cc7cd961bfa98ba2037b76af0ea427d7189c
Carlos Castañeda <carloscodvcf@gmail.com>
2024-09-16 18:16:55 +0200

    Update

--- PrimerasClases/dos.java
+++
@@ -1,7 +0,0 @@
-public class dos {
-    public static void main(String[] args) {
-        
-        System.out.println("Adiós Mundo!");
-        System.out.println("Eres genial");
-    }
-}
--- PrimerasClases/uno.java
+++
@@ -1,6 +0,0 @@
-public class uno {
-    public static void main(String[] args) {
-        
-        System.out.println("Hola Mundo!");
-    }
-}
