b826b7a68154d7e37a7978e4175502b1c9a8d573
Carlos Castañeda <carloscodvcf@gmail.com>
2024-09-18 20:27:08 +0200

    Update

---
+++ PrimerasClases/EjerciciosAmpliados/ampliado4.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class ampliado4 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String nom;
+
+        System.out.print("Dime tu nombre: ");
+        nom = sc.nextLine();
+
+        System.out.println("Buenos días, mi nombre es " + nom);
+    }
+}
---
+++ PrimerasClases/EjerciciosAmpliados/ampliado5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ampliado5 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String nom;
+        String loguitud;
+
+        System.out.print("Dime tu nombre: ");
+        nom = sc.nextLine();
+
+        loguitud = nom.length();
+
+    }
+}
