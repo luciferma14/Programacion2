c50451e3720253bdf72359a92eee34bd7dd239cf
Carlos Castañeda <carloscodvcf@gmail.com>
2024-09-19 20:20:49 +0200

    Update

--- PrimerasClases/EjerciciosAmpliados/ampliado5.java
+++ PrimerasClases/EjerciciosAmpliados/ampliado5.java
@@ -6,12 +6,21 @@ public class ampliado5 {
         Scanner sc = new Scanner(System.in);
 
         String nom;
-        String loguitud;
+        String ape1;
+        String ape2;
+        int tot;
 
         System.out.print("Dime tu nombre: ");
         nom = sc.nextLine();
 
-        loguitud = nom.length();
+        System.out.print("Dime tu primer apellido: ");
+        ape1 = sc.nextLine();
 
+        System.out.print("Dime tu segundo apellido: ");
+        ape2 = sc.nextLine();
+
+        tot = nom.length() + ape1.length() + ape2.length();
+
+        System.out.println("Total: " + tot);
     }
 }
---
+++ PrimerasClases/EjerciciosAmpliados/ampliado6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ampliado6 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String nom;
+        String[] separa;
+
+        System.out.print("Dime tu nombre completo: ");
+        nom = sc.nextLine();
+
+        separa = nom.split(" ",3);
+        
+        String n = separa[0];
+        String ap1 = separa[1];
+        String ap2 = separa[2];
+
+        int ln = separa[0].length();
+        int lap1 = separa[1].length();
+        int lap2 = separa[2].length();
+
+        System.out.printf("%s = %d letras \n%s = %d letras \n%s = %d letras\n", n, ln, ap1, lap1, ap2, lap2);
+    }
+}
