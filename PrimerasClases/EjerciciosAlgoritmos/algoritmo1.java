c5e977ffcc57d5ea94465bc442a3c585854658c3
Carlos Castañeda <carloscodvcf@gmail.com>
2024-09-30 18:10:33 +0200

    Update

---
+++ PrimerasClases/EjerciciosAlgoritmos/algoritmo1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class algoritmo1 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int num1;
+        int num2;
+
+        do{
+            System.out.print("Primero: ");
+            num1 = sc.nextInt();
+
+        }while(num1 <= 0);
+
+        do{
+            System.out.print("Segundo: ");
+            num2 = sc.nextInt();
+
+        }while(num2 <= 0);
+
+       if (num1 % num2 == 0){
+            System.out.println("Si es múltiplo");
+       }else {
+            System.out.println("No es múltiplo");
+       }
+    }
+}
