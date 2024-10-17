adf038403ddb21ac082b08c2161afe3078ccdfce
Lucia Ferrandis <luciferma14@gmail.com>
2024-10-17 11:40:50 +0200

    Update

--- PE/pe/pe6.java
+++ PE/pe/pe6.java
@@ -15,52 +15,74 @@ public class pe6 {
         String dia1s = "";
         String dia2s = "";
 
-        System.out.print("Día 1: ");
-        dia1 = sc.nextInt();
+        do{
+            System.out.print("Día 1: ");
+            dia1 = sc.nextInt();
 
-        System.out.print("Hora 1: ");
-        hora1 = sc.nextInt();
+            if (dia1 < 1 || dia1 > 7){
+                System.out.println("El día no es válido");
+            }
+        }while(dia1 < 1 || dia1 > 7);
 
-        System.out.print("Día 2: ");
-        dia2 = sc.nextInt();
+        do{
+            System.out.print("Hora 1: ");
+            hora1 = sc.nextInt();
 
-        System.out.print("Hora 2: ");
-        hora2 = sc.nextInt();
+            if (hora1 < 0 || hora1 > 23){
+                System.out.println("La hora no es válida");
+            }
 
-        if ( (dia1 >= 1 && dia1 <= 7) && (hora1 >= 0 && hora1 <= 23) 
-             &&
-             (dia2 >= 1 && dia2 <= 7) && (hora2 >= 0 && hora2 <= 23)
-             &&
-             dia1>=dia2 || (dia1 == dia2 && hora1 <= hora2) ){
+        }while(hora1 < 0 || hora1 > 23);
 
-            switch (dia1) {
-                case 1: dia1s = "lunes"; break;
-                case 2: dia1s = "martes"; break;
-                case 3: dia1s = "miércoles"; break;
-                case 4: dia1s = "jueves"; break;
-                case 5: dia1s = "viernes"; break;
-                case 6: dia1s = "sábado"; break;
-                case 7: dia1s = "domingo"; break;
-                default: dia1s = "Dia inválido"; break;
-                
-                }
-            switch (dia2) {
-                case 1: dia2s = "lunes"; break;
-                case 2: dia2s = "martes"; break;
-                case 3: dia2s = "miércoles"; break;
-                case 4: dia2s = "jueves"; break;
-                case 5: dia2s = "viernes"; break;
-                case 6: dia2s = "sábado"; break;
-                case 7: dia2s = "domingo"; break;
-                default: dia2s = "Dia inválido"; break;
-                
-            }            
+        do{
+            System.out.print("Día 2: ");
+            dia2 = sc.nextInt();
+
+            if(dia2 < dia1){
+                System.out.println("El día no es válido");
+            }
+
+            if (dia2 < 1 || dia2 > 7){
+                System.out.println("El día no es válido");
+            }
+        }while(dia2 < 1 || dia2 > 7);
+
+        do{
+            System.out.print("Hora 2: ");
+            hora2 = sc.nextInt();
+
+            if (hora2 < 0 || hora2 > 23){
+                System.out.println("La hora no es válida");
+            }
+
+        }while(hora2 < 0 || hora2 > 23);
+
+        switch (dia1) {
+            case 1: dia1s = "lunes"; break;
+            case 2: dia1s = "martes"; break;
+            case 3: dia1s = "miércoles"; break;
+            case 4: dia1s = "jueves"; break;
+            case 5: dia1s = "viernes"; break;
+            case 6: dia1s = "sábado"; break;
+            case 7: dia1s = "domingo"; break;
+            default: dia1s = "Dia inválido"; break;
+            
+        }
+        switch (dia2) {
+            case 1: dia2s = "lunes"; break;
+            case 2: dia2s = "martes"; break;
+            case 3: dia2s = "miércoles"; break;
+            case 4: dia2s = "jueves"; break;
+            case 5: dia2s = "viernes"; break;
+            case 6: dia2s = "sábado"; break;
+            case 7: dia2s = "domingo"; break;
+            default: dia2s = "Dia inválido"; break;
         }
 
-        horasTot1 = dia1 * 24 + hora1; // 1 * 24 + 12 = 36
-        horasTot2 = dia2 * 24 + hora2; // 2 * 24 = 48 + 13 = 61
+        horasTot1 = dia1 * 24 + hora1; // 1 * 24 + 18 = 42
+        horasTot2 = dia2 * 24 + hora2; // 2 * 24 = 48 + 20 = 68
 
-        horasTrans = horasTot2 - horasTot1;
+        horasTrans = horasTot2 - horasTot1; // 68 - 42 = 26
 
         System.out.printf("Entres las %d:00h del %s y las %d:00h del %s hay %d horas.\n", hora1, dia1s, hora2, dia2s, horasTrans);
     }
