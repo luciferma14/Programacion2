652df26fb5d43c2d5714210d519f60905e74b0a5
Lucia Ferrandis <luciferma14@gmail.com>
2024-10-25 13:53:17 +0200

    Update

--- PE/peExtra/peExtra5d.java
+++ PE/peExtra/peExtra5d.java
@@ -1,22 +1,46 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class peExtra5d {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-
-        int tam;
         
-        System.out.print("Tamaño del triángulo: ");
-        tam = sc.nextInt();
+        int numNiveles;
+        int numPosiciones;
+        int posInicial;
+        int posFinal;
+        int cont;
+        String resul;
 
-        for(int i = 1; i <= tam; i++){
-            System.out.println();
-            for(int j = 1; j <= i; j++){
-                System.out.print(i);
+        System.out.print("Niveles de la pirámide: ");
+        numNiveles = sc.nextInt(); // suponemos que es 4 (como ejemplo pdf ejercicios clase)
+        
+        numPosiciones = numNiveles * 2 -1; // 4 * 2 = 8 - 1 = 7 --> 1-2-3-4-3-2-1 = 7 números
+        posInicial = numNiveles;
+        posFinal = numNiveles;
+        
+        
+        for (int i = 0; i < numNiveles; i++) { // números de líneas --> 4 líneas
+            cont = 1;               
+            resul = "";
+            
+            for (int j = 0; j <= numPosiciones; j++) { // recorre las posiciones de "resultado"
+                if ((j < posInicial) || (j > posFinal)) {
+                    resul += " "; // resultado = resultado + " "
+                } else {
+                    if (j < numNiveles) {
+                        resul += cont; // resultado = resultado + contador
+                        cont++;
+                    } else {
+                        resul += cont;
+                        cont--;
+                    }
+                }    
             }
+            System.out.println(resul);
+            posFinal++;
+            posInicial--;
         }
-        System.out.println();
-
     }
 }
