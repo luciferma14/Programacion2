6739219c4629ce5f8d75f094cfadef5acdbffff6
Lucia <luciferma14@gmail.com>
2024-10-28 15:22:31 +0100

    Update

---
+++ PrimerasClases/arrays.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays;
+
+public class arrays {
+    public static void main(String[] args) {
+        
+        // int[] vec = new int[8];
+        int[] vec = {4,8,2,-5-2,7-3};
+
+        System.out.println(Arrays.toString(vec));
+
+
+
+
+    }
+}
