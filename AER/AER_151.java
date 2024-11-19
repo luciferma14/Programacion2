8d09a6a225c673f0faea6e69c4cda916641379c9
Lucia Ferrandis <luciferma14@gmail.com>
2024-11-19 20:01:02 +0100

    Update

---
+++ AER/AER_151.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AER_151 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int casos;
+        boolean sigue;
+        int valor;
+
+        casos = sc.nextInt();
+
+        do {
+
+            int[][] matriz = new int[casos][casos];
+
+            for(int i = 0; i < casos; i++){
+                for (int j = 0; j < casos; j++){
+
+                    matriz[i][j] = sc.nextInt();   
+                }   
+            }
+
+            sigue = false;
+            valor = 0;
+            casos = sc.nextInt();
+
+            valor++;
+                
+            if(matriz[valor][valor] == 1){
+                sigue = true;
+            }
+
+            if(sigue){
+                System.out.println("SI");
+            }else{
+                System.out.println("NO");
+            }
+
+        }while(casos != 0);
+    }
+}
