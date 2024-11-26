67511e37f9e73b17f7c2543375e5ce8dc2921646
Lucia Ferrandis <luciferma14@gmail.com>
2024-11-26 21:23:15 +0100

    Update

--- AER/AER_416.java
+++ AER/AER_416.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AER_416 {
@@ -7,13 +9,15 @@ public class AER_416 {
         Scanner sc = new Scanner(System.in);
 
         int casos;
+        boolean sigue = false;
         casos = sc.nextInt();
 
         do { 
             
             String[] fechas = new String[casos];
             String cumple = " ";
-            String[] barra = cumple.split("/");            
+            String[] barra = cumple.split("/"); 
+            List<String> lista = new ArrayList<String>();
 
             for (int i = 0; i < casos; i++) {
                 fechas[i] = sc.next();
@@ -21,26 +25,30 @@ public class AER_416 {
                 cumple = fechas[i];     
                 barra = cumple.split("/");
 
-               
-                
-                
                 // System.out.println(casos);
                 // System.out.println(Arrays.toString(fechas)); //todos los cumples
                 // System.out.println(cumple); //fecha del cumple
-                // System.out.print(barra[0] + ", "); //dia
+                //System.out.print(barra[0] + ", "); //dia
                 // System.out.println(barra[1] + ", "); //mes
                 // System.out.println(barra[2] + ", "); //año
 
+
+                lista.add(barra[0]);
+                System.out.println(barra[0]);
+
+                if (barra[0].equals(lista)){
+                    sigue = true;                
+                }else{
+                    sigue = false;
+                }
             }
-            System.out.println(barra.length);
-            for (int j = 0; j < barra.length; j++) {
-                for (int x = 0 ; x < barra.length; x++)
-                    if(barra[j] == barra[x]){
-                        System.out.println("SI");
-                    }else{
-                        System.out.println("NO");
-                    }
-                        
+
+            System.out.println(lista);
+
+            if (sigue){
+                System.out.println("SI");
+            }else{
+                System.out.println("NO");
             }
             
             // if ((barra[0].equals(barra[0])) && (barra[1].equals(barra[1]))){
