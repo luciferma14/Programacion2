539719d235a1dc7a8e277e2c241331f39ef9ad13
Lucia Ferrandis Martinez <luciaferrandismartinez@MacBook-Air-de-Lucia.local>
2025-01-02 22:24:57 +0100

    Update

--- ED/Ejer4.java
+++ ED/Ejer4.java
@@ -1,22 +1,38 @@
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Ejer4 {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
         LinkedList<Integer> listaNumeros = new LinkedList<Integer>();
         Queue<Integer> colaNumeros = new LinkedList<Integer>();
+
+        int opc;
 
         for(int i = 1; i <= 90; i++){
             listaNumeros.add(i);
         }
 
+        System.out.println("Números del BINGO");
+        System.out.println("Pulsa 1 para el siguiente número");
+        System.out.println("Pulsa 0 si ya has terminado");
+
         for(int i = 0; i < 90; i++){
             int random = (int) (Math.random() * 90);
             int saca = listaNumeros.get(random);
             colaNumeros.add(saca);
-            System.out.println(colaNumeros.poll());
         }
+
+        do{
+            opc = sc.nextInt();
+
+            if(opc == 1){
+                System.out.println(colaNumeros.poll());
+            }
+
+        }while (opc != 0);
     }
 }
