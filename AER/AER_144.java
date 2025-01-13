626c1b9ad6ade7d001306b5fb27d31a3dc36fcf2
Lucia Ferrandis Martinez <luciaferrandismartinez@MacBook-Air-de-Lucia.local>
2025-01-13 13:25:06 +0100

    Update

--- AER/AER_144.java
+++ AER/AER_144.java
@@ -1,10 +1,45 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AER_144 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
 
+        String texto;
+        int tres = 0;
+        char cada;
+
+        texto = sc.nextLine();
+
+        if(texto.contains("-") || texto.contains("+") || texto.contains("*") || texto.contains("3")){
+            // - inicio, + final, * flecha derecha, 3 suprimir
+            ArrayList<Character> cadena = new ArrayList<Character>();
+            ArrayList<Character> cadena2 = new ArrayList<Character>();
+            
+            
+            for(int i = 0; i < texto.length(); i++){
+                cada = texto.charAt(i);
+                // cuenta los treses que hay
+                if(cada == '3'){
+                    tres++;
+                }
+            }
+            for(int i = 0; i < texto.length(); i++){
+                cada = texto.charAt(i);
+                cadena.add(cada);
+                if(cada == '-'){
+                    for(int j = 0; j < tres; j++){
+                        cadena.removeFirst();
+                    }
+                }
+            }
+
+            
+            System.out.println(cadena);
+
+        }else{
+            System.out.println(texto);
+        }
     }
 }
