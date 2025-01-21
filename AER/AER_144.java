e6658718b89816fd3a9ee7d3a6f30731b5b5cc5e
Lucia Ferrandis Martinez <luciaferrandismartinez@MacBook-Air-de-Lucia.local>
2025-01-21 18:50:13 +0100

    Update

--- AER/AER_144.java
+++ AER/AER_144.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class AER_144 {
 
-    // falta terminarlo :(
+    // falta terminarlo, no se como :(
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -14,34 +14,37 @@ public class AER_144 {
 
         texto = sc.nextLine();
 
-        if(texto.contains("-") || texto.contains("+") || texto.contains("*") || texto.contains("3")){
-            // - inicio, + final, * flecha derecha, 3 suprimir
-            ArrayList<Character> cadena = new ArrayList<Character>();
-            ArrayList<Character> cadena2 = new ArrayList<Character>();
-            
-            
-            for(int i = 0; i < texto.length(); i++){
-                cada = texto.charAt(i);
-                // cuenta los treses que hay
-                if(cada == '3'){
-                    tres++;
-                }
-            }
-            for(int i = 0; i < texto.length(); i++){
-                cada = texto.charAt(i);
-                cadena.add(cada);
-                if(cada == '-'){
-                    for(int j = 0; j < tres; j++){
-                        cadena.removeFirst();
+        while(sc.hasNext()){
+
+            if(texto.contains("-") || texto.contains("+") || texto.contains("*") || texto.contains("3")){
+                // - inicio, + final, * flecha derecha, 3 suprimir
+                ArrayList<Character> cadena = new ArrayList<Character>();
+                ArrayList<Character> cadena2 = new ArrayList<Character>();
+                
+                
+                for(int i = 0; i < texto.length(); i++){
+                    cada = texto.charAt(i);
+                    // cuenta los treses que hay
+                    if(cada == '3'){
+                        tres++;
                     }
                 }
+                for(int i = 0; i < texto.length(); i++){
+                    cada = texto.charAt(i);
+                    cadena.add(cada);
+                    if(cada == '-'){
+                        for(int j = 0; j < tres; j++){
+                            cadena.removeFirst();
+                        }
+                    }
+                }
+
+                System.out.println(cadena);
+            }else{
+                System.out.println(texto);
             }
 
-            
-            System.out.println(cadena);
-
-        }else{
-            System.out.println(texto);
+            texto = sc.nextLine();
         }
     }
 }
---
+++ AER/AER_354.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AER_354 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        //NO ENTIENDO EL PROBLEMA
+    }
+}
