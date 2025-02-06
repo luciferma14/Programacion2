97aefc2b60bd8eebf04fc10973b34c4971826bd8
Lucia <luciferma14@gmail.com>
2025-02-06 17:22:24 +0100

    Update

---
+++ POO/Ejercicios/Incidencia.java
@@ -0,0 +1,75 @@
+public class Incidencia {
+    
+    private Integer puesto;
+    private String causa;
+    private static Integer aux = 1;
+    private Integer codigo;
+    private String estado;
+    private String resuelto;
+    private static Integer pendientes = 0;
+
+    public Incidencia(){
+        this.estado = "Pendiente";
+        this.codigo = aux++;
+        pendientes++;
+    }
+
+    public Incidencia(Integer puesto, String causa){
+        this.puesto = puesto;
+        this.causa = causa;
+        this.estado = "Pendiente";
+        this.codigo = aux++;
+        pendientes++;
+    }
+
+    public void resuelve(String resuelto){
+        if(this.estado.equals("Resuelta")){
+            System.out.println("Esta incidencia ya está resuelta");
+        }else{
+            this.resuelto = resuelto;
+            this.estado = "Resuelta";
+            pendientes--;
+        }
+    }
+
+    public Integer getPuesto() {
+        return this.puesto;
+    }
+
+    public void setPuesto(Integer puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getCausa() {
+        return this.causa;
+    }
+
+    public void setCausa(String causa) {
+        this.causa = causa;
+    }
+
+    public Integer getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public static Integer getPendientes() {
+        return Incidencia.pendientes;
+    }
+
+    public void setPendientes(Integer pendientes) {
+        this.pendientes = pendientes;
+    }
+
+    public String toString(){
+        if (this.resuelto != null){
+            return "Incidencia " + this.codigo + " - Puesto: " + this.puesto + " - " + this.causa + " - " + this.estado + " - " + this.resuelto;
+        }else{
+            return "Incidencia " + this.codigo + " - Puesto: " + this.puesto + " - " + this.causa + " - " + this.estado;
+        }
+     
+    }
+}
