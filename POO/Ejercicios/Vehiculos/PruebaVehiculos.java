ebb4361fcf60cf427cef19f37988c484ba97f3ce
Lucia <luciferma14@gmail.com>
2025-03-10 17:19:23 +0100

    Update

--- POO/Ejercicios/Vehiculos/PruebaVehiculos.java
+++ POO/Ejercicios/Vehiculos/PruebaVehiculos.java
@@ -1,86 +1,88 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PruebaVehiculos {
 
-    public static int menu(){
-        int opc = 0;
-        System.out.println("VEHÍCULOS");
-        System.out.println("=========");
-        System.out.println("1. Anda en bicicleta");
-        System.out.println("2. Anda en coche");
-        System.out.println("3. Ver kilometraje de la bicicleta");
-        System.out.println("4. Ver kilometraje del coche");
-        System.out.println("5. Ver kilometros totales");
-        System.out.println("6. Ver vehículos totales");
-        System.out.println("7. Salir");
-
-        return opc;
-    }
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int op;
-        int opc = 0;
-
-        Bicicleta bici = new Bicicleta();
-        Coche coche = new Coche();
+        ArrayList<Bicicleta> bicis = new ArrayList<>();
+        ArrayList<Coche> coches = new ArrayList<>();
+        int opc;
 
         do { 
             
             System.out.println("VEHÍCULOS");
             System.out.println("=========");
-            System.out.println("1. Anda en bicicleta");
-            System.out.println("2. Anda en coche");
-            System.out.println("3. Ver kilometraje de la bicicleta");
-            System.out.println("4. Ver kilometraje del coche");
-            System.out.println("5. Ver kilometros totales");
-            System.out.println("6. Ver vehículos totales");
-            System.out.println("7. Salir");
+            System.out.println("1. Añadir bicicleta");
+            System.out.println("2. Añadir coche");
+            System.out.println("3. Anda en bicicleta");
+            System.out.println("4. Anda en coche");
+            System.out.println("5. Ver kilometraje de la bicicleta");
+            System.out.println("6. Ver kilometraje del coche");
+            System.out.println("7. Ver kilometros totales");
+            System.out.println("8. Ver vehículos totales");
+            System.out.println("9. Salir");
+            System.out.print("Elige una opción (1-9): ");
 
             opc = sc.nextInt();
 
-            //op = menu();
-
             switch (opc) {
                 case 1:
-                    System.out.println("Cuantos kilometros anda??");
-                    int andaBici = sc.nextInt();
-                    bici.anda(andaBici);
-                    
+                    Bicicleta bicii = new Bicicleta();
+                    bicis.add(bicii);
                     break;
-                case 2:
-                    System.out.println("Cuantos kilometros anda??");
-                    int andaCoche = sc.nextInt();
-                    coche.anda(andaCoche);
                     
+                case 2:
+                    Coche cochee = new Coche();
+                    coches.add(cochee);
                     break;
 
                 case 3:
-                    System.out.println(bici.getKilometrosRecorridos());
-                    
+                    System.out.println("Cuantos kilometros anda??");
+                    int andaBici = sc.nextInt();
+                    int bic = (int) (Math.random()* (bicis.size()));
+                    bicis.get(bic).anda(andaBici);
                     break;
-                case 4: 
-                    System.out.println(coche.getKilometrosRecorridos());
 
+                case 4:
+                    System.out.println("Cuantos kilometros anda??");
+                    int andaCoche = sc.nextInt();
+                    int coc = (int) (Math.random()* (coches.size()));
+                    coches.get(coc).anda(andaCoche); 
                     break;
+
                 case 5:
-                    System.out.println(Vehiculo.getKilometrosTotales());
-
+                    for (Bicicleta bicicleta : bicis) {
+                        System.out.println(bicicleta.getKilometrosRecorridos());
+                    }
                     break;
+
                 case 6: 
-                    System.out.println(Vehiculo.getVehiculosCreados());
-
+                    for (Coche coche : coches) {
+                        System.out.println(coche.getKilometrosRecorridos());
+                    }
                     break;
+
                 case 7:
+                    System.out.println(Vehiculo.getKilometrosTotales());
+                    break;
+
+                case 8: 
+                    System.out.println(Vehiculo.getVehiculosCreados());
+                    break;
+
+                case 9:
                     System.out.println("Saliendo...");
                     System.out.println("Adioss :)");
                     System.exit(0);
+
                 default:
+                    System.out.println("Opción no válida :(");
+                    break;
             }
 
-            //menu();
-
-        } while(opc != 7);
+        } while(opc != 9);
     }
 }
