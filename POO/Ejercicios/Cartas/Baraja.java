9070e1f8783322a2ae7f82e69911262cb2cc7ff3
Lucia <luciferma14@gmail.com>
2025-03-25 17:22:06 +0100

    Update

--- POO/Ejercicios/Cartas/Baraja.java
+++ POO/Ejercicios/Cartas/Baraja.java
@@ -5,8 +5,9 @@ public class Baraja {
 
     private ArrayList<Carta> cartas = new ArrayList<>();
 
-    public Baraja(int numeros){
-        for(int i = 1; i <= numeros; i++){
+    public Baraja() {
+        // Crear cartas con los 4 palos y valores de 1 a 12
+        for (int i = 1; i <= 12; i++) {
             cartas.add(new Carta(i, "Espadas"));
             cartas.add(new Carta(i, "Oros"));
             cartas.add(new Carta(i, "Copas"));
@@ -14,20 +15,28 @@ public class Baraja {
         }
     }
 
-    public void barajar(){
+    public void barajar() {
         for (int i = 0; i < 200; i++) {
-            int posi = (int) (Math.random()* cartas.size());
+            int posi = (int) (Math.random() * cartas.size());
             Carta aux = cartas.remove(posi);
             cartas.add(aux);
         }
     }
 
-    public void barajar2(){
+    public void barajar2() {
         Collections.shuffle(cartas);
     }
 
-    public Carta repartirCarta(){
-        return  cartas.remove(0);
+    // Reparte las cartas equitativamente entre los jugadores
+    public ArrayList<ArrayList<Carta>> repartirCartas(int numJugadores) {
+        ArrayList<ArrayList<Carta>> manos = new ArrayList<>();
+        for (int i = 0; i < numJugadores; i++) {
+            manos.add(new ArrayList<>());
+        }
+        for (int i = 0; i < cartas.size(); i++) {
+            manos.get(i % numJugadores).add(cartas.get(i));
+        }
+        return manos;
     }
 
     public ArrayList<Carta> getCartas() {
