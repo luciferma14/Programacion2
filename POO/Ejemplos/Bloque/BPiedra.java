ae35d18233e97bc26c0753fea833d978cd8b795c
Lucia <luciferma14@gmail.com>
2025-03-31 18:24:38 +0200

    Update

--- POO/Ejemplos/Bloque/BPiedra.java
+++ POO/Ejemplos/Bloque/BPiedra.java
@@ -2,6 +2,7 @@ public class BPiedra extends Bloque{
     
     public BPiedra(){
         super();
+        setColor("Gris");
     }
 
     public BPiedra(Integer alto, Integer ancho, Integer fondo, String color){
